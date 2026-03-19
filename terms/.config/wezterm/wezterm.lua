613ec3e20a520e4f98ac42d98fba1d8bd9161892
Salmi, Mahmoud <mahmoud@salmi.dev>
2026-03-19 06:52:33 +0100

    update wezterm config

--- terms/.config/wezterm/wezterm.lua
+++ terms/.config/wezterm/wezterm.lua
@@ -7,12 +7,13 @@ local function font_with_fallback(font_family)
   -- family names, not file names
   return wezterm.font_with_fallback({
     font_family,
+    "Noto Naskh Arabic",         -- for Arabic script
     "Font Awesome 6 Free Solid", -- nice double-spaced symbols!
   })
 end
 
 local function calculate_font_size(uname, osname)
-  wezterm.log_info ("UNAME = "..uname..", OSNAME = "..osname)
+  wezterm.log_info("UNAME = " .. uname .. ", OSNAME = " .. osname)
   if osname == 'macos' then
     return 14.0
   end
@@ -20,8 +21,10 @@ local function calculate_font_size(uname, osname)
 end
 
 
-config.font_dirs = {"fonts"}
+config.font_dirs = { "fonts" }
 config.font = font_with_fallback("Monaspace Neon")
+--config.font = font_with_fallback("Noto Sans Arabic")
+
 config.font_size = calculate_font_size(utils.os_name())
 config.harfbuzz_features = {
   "zero", -- Use a slashed zero '0' (instead of dotted)
@@ -30,6 +33,7 @@ config.harfbuzz_features = {
   "clig", -- (default) contextual ligatures
   'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'calt', 'dlig'
 }
+config.bidi_enabled = true
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
@@ -37,10 +41,10 @@ config.tab_bar_at_bottom = true
 
 local padding_space = 3
 config.window_padding = {
-  left    = padding_space,
-  right   = padding_space,
-  top     = padding_space,
-  bottom  = padding_space,
+  left   = padding_space,
+  right  = padding_space,
+  top    = padding_space,
+  bottom = padding_space,
 }
 
 config.window_background_opacity = 0.97
@@ -57,4 +61,3 @@ config.scrollback_lines = 100000
 config.enable_scroll_bar = true
 
 return config
-
